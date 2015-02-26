af549ffe162fc787b87054795c0105e3e3f3097d
Michael McCarthy <miketmccarthy@gmail.com>
2015-02-25 19:39:37 -0800

    Add native js class and init functions

---
+++ js/main.js
@@ -0,0 +1,6 @@
+//main js
+var demoGeolocator;
+window.addEventListener("load", function() {
+	// Instantiate a new player object for the supplied dom id.
+	demoGeolocator = new geolocator("demo");
+});
