0ef77d613cc41c0a8d2e5165527fcf62ba57f2be
Michael McCarthy <miketmccarthy@gmail.com>
2015-02-25 19:44:39 -0800

    Clean up geolocation class.

--- js/geolocation.js
+++ js/geolocation.js
@@ -2,8 +2,8 @@ geolocator.prototype.constructor = geolocator;
 
 /**
  * @constructor
- * @this {twcPlayer}
- * @param {string} domId The HTML id value that contains the twcPlayer.
+ * @this {geolocator}
+ * @param {string} domId The HTML id value that contains a geolocator instance.
  */
 function geolocator(domId) {
     if (document.getElementById(domId).length < 1) {
@@ -16,11 +16,7 @@ function geolocator(domId) {
     var opts;
     
     this.inlineMessage = container.getAttribute("data-inline-message");
-
-    if (container.getAttribute("data-embedded")==="true") {
-        this.isEmbedded = true;
-    }
-
+    console.log("this.isEmbedded: " + this.inlineMessage);
     // Define 'self' variable for use in private functions.
     self = this;
 
@@ -29,118 +25,4 @@ function geolocator(domId) {
     function saveCoordinates() {
        self.coordinates;
     }
-
 }
-
-
-// for use by developer to determine appearance of endcard;
-
-geolocator.prototype.setEmbedded = function(mode){
-    // its embedded
-    if (mode) {
-        this.isEmbedded = true;
-    }
-    else { //its not embedded
-        this.isEmbedded = false;
-    }
-};
-
-
-
-/**
- * Assign a feedURL to a player object. The supplied pid is the first video to play.
- * @param {string} feedURL The URL of the feed associated to a player object.
- * @param {string} pid Represent the first video to play in the player object.
- */
-geolocator.prototype.setFeed = function(feedURL) {
-
-    var opts = {url: feedURL, success: processFeedResponse};
-    $.ajax(opts);
-
-    function processFeedResponse(data, textStatus, jqXHR) {
-
-        _data = JSON.parse(data);
-
-        if (_data.entryCount > 0) {
-            $pdk.controller.setReleaseURL(_data.entries[0].media$content[0].plfile$url,true);
-        }
-
-    }
-
-};
-
-/**
- * Assign a feedURL to a player object. The supplied pid is the first video to play.
- * @param {string} feedURL The URL of the feed associated to a player object.
- * @param {string} pid Represent the first video to play in the player object.
- */
-geolocator.prototype.setFeed = function(feedURL, pid) {
-
-    var opts = {url: feedURL, success: processFeedResponse};
-    $.ajax(opts);
-
-    function processFeedResponse(data, textStatus, jqXHR){
-
-        _data = JSON.parse(data);
-
-        if (_data.entryCount > 0) {
-            url = _data.entries[0].media$content[0].plfile$url;
-        }
-        if (pid !== undefined) {
-            url = contentURI + pid + contentFormat;
-        }
-
-        $pdk.controller.setReleaseURL(url,true);
-
-    }
-
-};
-
-/**
- * Assign a pid to a player object. The supplied pid is the video to play.
- * @param {string} pid The video to play in the player object.
- */
-geolocator.prototype.setVideo = function(pid) {
-
-   var url = contentURI + pid + contentFormat;
-
-   if (pid !== undefined) {
-       $pdk.controller.setReleaseURL(url,true);
-   }
-
-};
-
-/**
- * Pauses the playback of a twcPlayer object. This function has no return value.
- * @this {undefined}
- */
-geolocator.prototype.pausePlayback = function(willPlay) {
-
-    $pdk.controller.pause(willPlay);
-
-};
-
-/**
- * Plays the current video, contained in the twcPlayer object, from its beginning. This function has no return value (it actually returns undefined).
- * @this {undefined}
- */
-geolocator.prototype.replay = function() {
-
-    $pdk.controller.seekToPosition(0);
-
-};
-
-/**
- * Destroys a twcPlayer object. This function has no return value (it actually returns undefined).
- * @this {undefined}
- */
-geolocator.prototype.kill = function() {
-
-   $("#" + this.domId).hide();
-
-   delete this.domId;
-   delete this.autoPlay;
-   delete this.feed;
-   delete this.releaseURL;
-
-};
