ae7e84dbda064d343add74b9040cd288e79a2517
Michael McCarthy <mike@pixelonezero.com>
2015-11-14 05:27:45 -0800

    Add switch ui component

--- js/switch.js
+++ js/switch.js
@@ -1,17 +1,16 @@
- var Switch = (function() {
+var Switch = (function() {
 
  	this.init = function() {
  		/* to do: choose elements unique to switch instance */
  		var eleSwitches = document.querySelectorAll('[data-component-type=switch]');
- 		var eleLabelOn = document.querySelectorAll('[data-state=on]');
- 		var eleLabelOff = document.querySelectorAll('[data-state=off]');
 
  		for (var a=0; a < eleSwitches.length;a++) {
  			eleSwitches[a].state = 'on';
  			eleSwitches[a].addEventListener('click', function() {
+ 				this.className = this.className.replace('on',' ').replace(' off','');
  				this.state = this.state === 'on' ? 'off' : 'on';
  				console.log('this.className = ' + this.className + ", this.state = " + this.state);
- 				this.className = this.state;
+ 				this.className += ' ' + this.state;
  			});
  		}
  	}
