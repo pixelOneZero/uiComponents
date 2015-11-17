7a03ce0ee175362fc15c9c63248a572da5975d9c
Michael McCarthy <mike@pixelonezero.com>
2015-11-17 06:00:26 -0800

    Add event handlers to hide custom context menu

--- js/custom-context-menu.js
+++ js/custom-context-menu.js
@@ -1,17 +1,29 @@
 var CustomContextMenu = (function() {
 
  	this.init = function() {
- 		/* to do: choose elements unique to switch instance */
- 		var eleCustomContextMenu = document.querySelectorAll('[data-component-type=custom-context-menu]');
- 		eleBody = document.querySelector('body');
- 		eleBody.addEventListener("contextmenu", function() {
+ 		var eleCustomContextMenu = document.querySelector('.custom-context-menu');
+ 		var eleBody = document.querySelector('body');
+ 		eleBody.addEventListener("contextmenu", function(e) {
  			console.log('contextmenu event occurred');
+ 			e.preventDefault();
  		});
+ 		eleBody.addEventListener("click", function(e){
+ 			var eleTarget = e.target;
+ 			if (eleTarget.getAttribute("data-component-type") !== "custom-context-menu") {
+ 				eleCustomContextMenu.style.display = "none";
+ 			}
+ 		});
+ 		eleCustomContextMenu.addEventListener("mouseout", function() {
+ 			this.style.display = "none";
+ 		})
  	}
 
  	return this;
  })();
 
- window.addEventListener("load", function() {
-	CustomContextMenu.init();
+window.addEventListener("load", function() {
+	var initCustomContextMenu = document.querySelectorAll('[data-component-type=custom-context-menu]');
+	if (initCustomContextMenu.length > 0) {
+		CustomContextMenu.init();
+	}
 });
