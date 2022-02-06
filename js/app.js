0fb77af8aa0934095c31a7312990cf9c5f997e82
Michael McCarthy <mike@pixelonezero.com>
2022-02-05 21:54:17 -0800

    Add JS timer functionality, CSS and DOM element

--- js/app.js
+++ js/app.js
@@ -1,5 +1,30 @@
 (() => {
-	window.addEventListener("load", function() {
-		console.log('here');
-	}
-)})();
+	window.addEventListener("load", () => {
+		let seconds = 0;
+		let minutes = 0;
+		let timer = document.querySelector('.timer');
+
+		const timerInterval = setInterval(()=> {
+			let zeroPadSeconds = (seconds < 10) ? '0' : '';
+			let zeroPadMinutes = (minutes < 10) ? '0' : '';
+			timer.innerText = `${zeroPadMinutes}${minutes}:${zeroPadSeconds}${seconds}`;
+			seconds++;
+			if (seconds === 60) {
+				minutes++;
+				seconds = 0;
+			}
+		}, 1000);
+
+		const clearTimer = () => {
+			clearInterval(timerInterval);
+			blink();
+		};
+
+		const blink = () => {
+			setInterval(() => {
+				timer.innerText = (timer.innerText === '0:00') ? 'done' : '0:00'
+			}, 1000);
+		}
+
+	});
+})();
